bbe6334a201f6c15ced619404144417b6f83ff7c
Lenny Capellan <104685438+git719@users.noreply.github.com>
2025-01-31 15:02:32 -0500

    include vault binary

--- Dockerfile
+++ Dockerfile
@@ -1,8 +1,10 @@
 # Build image locally with:
-#   docker build --no-cache -t local-ubi9:x.x.x .
+#   docker build -f Dockerfile --no-cache -t local-cntimage:latest .
 
 # Use AlmaLinux minimal image as the base
 FROM almalinux/9-minimal
+
+ADD requirements.txt /app/requirements.txt
 
 # Install essential tools (customize as needed)
 COPY layer1.sh /tmp/layer1.sh
--- layer1.sh
+++ layer1.sh
@@ -6,6 +6,53 @@ set -euo pipefail
 # Install required packages
 microdnf update -y
 microdnf install -y \
-git
+git \
+glibc \
+python3-libs \
+python3.12 \
+python3.12-devel \
+python3.12-pip \
+jq \
+which \
+iputils \
+iproute \
+python3.12-cffi \
+cairo \
+pango \
+gdk-pixbuf2 \
+libffi \
+shared-mime-info
+# Append/add other required packages here
 microdnf clean all
+rm -rf /var/cache/dnf/*
 
+# ---------------------------------------
+# Create a new repo file for HashiCorp and install Vault
+cat <<EOF > /etc/yum.repos.d/hashicorp.repo
+[hashicorp]
+name=HashiCorp Stable
+baseurl=https://rpm.releases.hashicorp.com/RHEL/9/x86_64/stable
+enabled=1
+gpgcheck=1
+gpgkey=https://rpm.releases.hashicorp.com/gpg
+EOF
+#microdnf repoquery vault # To search available
+microdnf install -y vault-1.16.3
+
+# ---------------------------------------
+# Set up Python 3.12 as default
+# Set up python links
+alternatives --install /usr/bin/python3 python3 /usr/bin/python3.12 1
+alternatives --install /usr/bin/python python /usr/bin/python3.12 1
+alternatives --set python3 /usr/bin/python3.12
+alternatives --set python /usr/bin/python3.12
+# Set up pip links
+alternatives --install /usr/bin/pip3 pip3 /usr/bin/pip3.12 1
+alternatives --install /usr/bin/pip pip /usr/bin/pip3.12 1
+alternatives --set pip3 /usr/bin/pip3.12
+alternatives --set pip /usr/bin/pip3.12
+# Update pip and process requirements.txt
+pip install --upgrade pip
+pip install -r /app/requirements.txt
+
+exit 0
