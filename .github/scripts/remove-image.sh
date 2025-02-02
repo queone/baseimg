70ac0f19e108388de5b2003b092a0403b4d2e667
Lenny Capellan <104685438+git719@users.noreply.github.com>
2025-02-02 07:40:05 -0500

    Refactored image workflows

--- .github/scripts/remove-image.sh
+++ .github/scripts/remove-image.sh
@@ -3,11 +3,12 @@
 set -e
 
 if [ -z "$1" ]; then
-  echo "Usage: $0 <package-name>"
+  echo "Usage: $0 <package-name> <version>"
   exit 1
 fi
 
 PACKAGE_NAME=$1
+VERSION=$2
 USERNAME="queone"
 TOKEN="${GH_TOKEN}"
 
@@ -23,18 +24,21 @@ RESPONSE=$(curl -s -H "Authorization: Bearer $TOKEN" \
 
 echo "API Response: $RESPONSE"
 
-IMAGES=$(echo "$RESPONSE" | jq -r 'map(select(.id)) | .[].id')
+# Find the specific version ID
+VERSION_ID=$(echo "$RESPONSE" | jq -r --arg VERSION "$VERSION" '.[] | select(.name == $VERSION) | .id')
 
-if [[ -z "$IMAGES" ]]; then
-  echo "No images found."
-else
-  for IMAGE_ID in $IMAGES; do
-    echo "Deleting image ID: $IMAGE_ID"
-    curl -X DELETE -H "Authorization: Bearer $TOKEN" \
-      -H "Accept: application/vnd.github.v3+json" \
-      "https://api.github.com/orgs/$USERNAME/packages/container/$PACKAGE_NAME/versions/$IMAGE_ID"
-  done
+if [[ -z "$VERSION_ID" ]]; then
+  echo "No image found for version: $VERSION"
+  exit 1
 fi
+
+# Delete the specific version
+echo "Deleting image ID: $VERSION_ID for version: $VERSION"
+curl -X DELETE -H "Authorization: Bearer $TOKEN" \
+  -H "Accept: application/vnd.github.v3+json" \
+  "https://api.github.com/orgs/$USERNAME/packages/container/$PACKAGE_NAME/versions/$VERSION_ID"
+
+echo "Version $VERSION of package $PACKAGE_NAME deleted successfully."
 
 # Delete the package itself
 echo "Deleting package: $PACKAGE_NAME"
