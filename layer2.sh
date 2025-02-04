d53cb55cb106e10ed453477866e213ce9b0784ad
Lenny Capellan <104685438+git719@users.noreply.github.com>
2025-02-04 18:32:08 -0500

    Updated layer2 script for vault to support multi-arch

--- layer2.sh
+++ layer2.sh
@@ -8,17 +8,20 @@ set -euo pipefail
 cat <<EOF > /etc/yum.repos.d/hashicorp.repo
 [hashicorp]
 name=HashiCorp Stable
-baseurl=https://rpm.releases.hashicorp.com/RHEL/9/x86_64/stable
+baseurl=https://rpm.releases.hashicorp.com/RHEL/9/\$basearch/stable
 enabled=1
 gpgcheck=1
 gpgkey=https://rpm.releases.hashicorp.com/gpg
 EOF
-#microdnf repoquery vault # To search available
+
+# Update and install Vault
 microdnf update -y
-microdnf install -y libcap vault-1.18.4 # Needed by Vault
+microdnf install -y libcap vault # Install the latest available version
 microdnf clean all
 rm -rf /var/cache/dnf/*
+
 # Vault requires IPC_LOCK capability for memory locking
 setcap cap_ipc_lock=+ep /usr/bin/vault
 
 exit 0
+
